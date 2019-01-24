7ca97080f4075eb31a212b3937948f6188212899
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-24 20:00:41 +0700

    Create FileManager Back-end.

---
+++ src/FileManager.BackEnd/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // GET api/files
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/files/<filename>
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/files
+        [HttpPost]
+        public void Post(IFormFile file)
+        {
+        }
+
+        // DELETE api/files/<filename>
+        [HttpDelete("{filename}")]
+        public void Delete(string filename)
+        {
+        }
+    }
+}
---
+++ src/FileManager.BackEnd/Models/FileManagerOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.BackEnd.Models
+{
+    public class FileManagerOptions
+    {
+
+        public string Directory { get; set; }
+    }
+}
