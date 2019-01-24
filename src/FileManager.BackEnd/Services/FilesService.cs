eeae0b05cb8cbbcec3d504ad6a49cf6f01e1b8af
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-24 20:43:11 +0700

    Add files management service and options.

---
+++ src/FileManager.BackEnd/Services/FilesService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileManager.BackEnd.Models;
+
+namespace FileManager.BackEnd.Services
+{
+    public class FilesService
+    {
+        private readonly FileManagerOptions _options;
+
+        public FilesService(FileManagerOptions options)
+        {
+            _options = options;
+        }
+    }
+}
--- src/FileManager.BackEnd/Startup.cs
+++ src/FileManager.BackEnd/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FileManager.BackEnd.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,8 @@ namespace FileManager.BackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<FileManagerOptions>(Configuration.GetSection("FileManager"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
