87bb04e28c016bedeaabd51d01e3e427252d19f3
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-27 01:27:05 +0700

    - Implement auto-icon setter- fixes.

--- src/FileManager.FrontEnd/js/main.js
+++ src/FileManager.FrontEnd/js/main.js
@@ -1,21 +1,31 @@
 //
 var File = function (data) {
     
-    this.name = data.filename;
-    this.size = data.size;
-    this.lastModified = data.lastModified;
-    this.location = data.location;
+    var self = this;
 
-    this.icon = (function () {
-        
-    })();
+    self.name = data.filename;
+    self.size = data.size;
+    self.modified = data.lastModified;
+    self.location = data.location;
+
+    var iconFromFilename = function (filename) {
+        if(filename.endsWith("txt")) {
+            return "images/txt-file-icon.png";
+        } else if(filename.endsWith("jpg") || filename.endsWith("jpeg")) {
+            return "images/jpg-file-icon.png";
+        } else {
+            return "images/file-icon.png";
+        }
+    }
+
+    this.icon = iconFromFilename(self.name);
 
     this.delete = function () {
-        
+        console.log("Delete file: ", self.name);
     };
 
     this.view = function () {
-        
+        console.log("View file: ", self.name);        
     };
 }
 
@@ -23,10 +33,10 @@ var FileManagerViewModel = function () {
     var self = this;
 
     self.files = ko.observableArray([
-        { icon: "images/file-icon.png", filename: "index.js", size: filesize(128), lastModified: moment("20190115", "YYYYMMDD").fromNow() },
-        { icon: "images/file-icon.png", filename: "page.html", size: filesize(2049), lastModified: moment("20190110", "YYYYMMDD").fromNow() },
-        { icon: "images/file-icon.png", filename: "style.css", size: filesize(3650), lastModified: moment("20181215", "YYYYMMDD").fromNow() },
-        { icon: "images/file-icon.png", filename: "ReadMe.txt", size: filesize(4096), lastModified: moment("20190120", "YYYYMMDD").fromNow() }
+        new File({ filename: "index.jpeg", size: filesize(128), lastModified: moment("20190115", "YYYYMMDD").fromNow() }),
+        new File({ filename: "page.txt", size: filesize(2049), lastModified: moment("20190110", "YYYYMMDD").fromNow() }),
+        new File({ filename: "style.jpg", size: filesize(3650), lastModified: moment("20181215", "YYYYMMDD").fromNow() }),
+        new File({ filename: "ReadMe.txt", size: filesize(4096), lastModified: moment("20190120", "YYYYMMDD").fromNow() })
     ]);
 
     self.file = ko.observable();
@@ -34,17 +44,10 @@ var FileManagerViewModel = function () {
         self.upload();
     });
 
-    self.view = function () {
-        
-    };
-
     self.upload = function () {
-        console.log("Value of FILE: ", self.file());
+        console.log("Upload file: ", self.file());
     };
 
-    self.delete = function () {
-        
-    };
 };
 
 (function() {
