db053ef3f24609bf3a69cfa9f39070aecf1cc19e
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-30 20:01:06 +0700

    WIP: refactor view model and logic.

--- src/FileManager/wwwroot/js/main.js
+++ src/FileManager/wwwroot/js/main.js
@@ -4,72 +4,98 @@ var File = function (data)
 
     var self = this;
 
-    self.name = data.fileName;
+    self.name = data.name;
     self.size = filesize(data.size);
-    self.modified = moment(data.lastModified).fromNow();
+    self.modified = moment(data.lastModifiedDate).fromNow();
     self.location = data.location;
-    self.extension = self.name.split(".").pop().toLowerCase();
+    self.type = data.type;
     self.icon = (function ()
     {
-        if (self.extension === "txt")
-        {
-            return "assets/txt-file-icon.png";
-        }
-        else if (self.extension === "jpg" || self.extension === "jpeg")
-        {
-            return "assets/jpg-file-icon.png";
-        }
-        else
-        {
-            return "assets/file-icon.png";
-        }
+        return "assets/file-icon.png";
     })();
 
 };
 
-var FileManagerViewModel = function () {
-    
+var upload = function (url, data, callbacks)
+{
+    axios.post(url, data)
+        .then(callbacks.success)
+        .catch(callback.fail);
+};
+
+var remove = function (url, data, callbacks)
+{
+    axios.delete(url, data)
+         .then(callbacks.success)
+         .catch(callbacks.fail);
+};
+
+var download = function (url, fail)
+{
+    axios.get(apiUrl)
+        .then(function (response)
+        {
+            self.files(response.data.map(function (element)
+            {
+                return new File(element);
+            }));
+        })
+        .catch(fail);
+};
+
+function FilesViewModel() {
     var self = this;
-    var apiUrl = "/api/files";
+    
+    // api endpoint.
+    self.apiUrl = "/api/files/";
 
-    self.getfiles = function ()
-    {
-        axios.get(apiUrl)
-             .then(function (response) {
-                self.files(response.data.map(function (element) {
-                    return new File(element);
-                }));
-             })
-             .catch(function (error) {
-                 // TODO:
-             });
+    // properties.
+
+    // files list.
+    self.files = ko.observableArray();
+
+    // viewable content-types.
+    self.supportedContentTypes = [ "text/plain", "image/jpeg" ];
+
+    self.viewable = function (file) {
+        return self.supportedContentTypes.indexOf(file.type) !== -1;
     };
 
-    // Files list.
-    self.files = ko.observableArray(self.getfiles());
+    // delete file.
+    self.delete = function (file) {
 
-    // File to upload.
-    self.file = ko.observable();
-    self.file.subscribe(function () {
-        self.upload();
-    });
-
-    const viewable = ["txt", "jpg", "jpeg"];
-
-    // Check if file of viewable type.
-    self.canView = function (extension)
-    {
-        return viewable.indexOf(extension) != -1;
     };
 
-    // Retrieve files from server.
-    self.refersh = function () {
-        self.files(self.getfiles());
+    // view file.
+    self.view = function (file) {
+        
     };
+
+    // upload selected file.
+    self.send = function (file) {
+        file.location = self.apiUrl.concat(file.name);
+        var newFile = new File(file);
+        self.files.push(newFile);
+
+        // TODO: Send to server.
+    };
+};
+
+var FileManagerViewModel = function () {
 
     // Upload file to server.
     self.upload = function () {
 
+        var selectedFile = document.getElementById("file").files[0];
+        console.log(selectedFile);
+
+        self.files().push(new File({
+            fileName: selectedFile.name,
+            size: selectedFile.size,
+            modified: selectedFile.lastModified
+        }));
+
+        /*
         var data = new FormData();
         data.append("File", document.getElementById("file").files[0]);
 
@@ -79,28 +105,8 @@ var FileManagerViewModel = function () {
              })
              .catch(function (error) {  
                  // TODO:
-             });
+             });*/
     };
-
-    // Delete file from server.
-    self.delete = function (file) {
-
-        var data = {
-            data: file.name
-        };
-
-        axios.delete(apiUrl + "\\" + file.name, data)
-             .then(function () {
-            self.refersh();
-        });
-    };
-
-    // Open new window to view a file.
-    self.view = function (file) {
-        var preview = window.open(file.location);
-        preview.focus();
-    };
-
 };
 
 (function() {
